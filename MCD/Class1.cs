fabaf64b0135d81aa46b36c50183b84ebf5b0640
Ricklm <73793126@continental.edu.pe>
2024-05-04 15:17:08 -0500

    Ejercicio MCD C#

---
+++ MCD/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD
+{
+    public class Class1
+    {
+        public static double mcd(double nro1,double nro2)
+        {
+           if(nro2==0)
+            {
+                return nro1;
+            }else
+            {
+                return mcd(nro2, nro1 % nro2);
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using MCD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MCD;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MCD()
+        {
+            double nro1 = 24;
+            double nro2 = 36;
+            double esperado = 12;
+            double prueba = Class1.mcd(nro1, nro2);
+            Assert.AreEqual(esperado, prueba);
+        }
+    }
+}
